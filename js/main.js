6eadc4192e4e7c80d34c4a362c15251d0045258f
anabrollan <anabrollan@gmail.com>
2024-07-29 13:10:45 -0400

    Creo el objeto en js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-const newBook = {author, title, date, finished}
+const newBook = {}
+
+newBook.author = prompt("Ingresa el autor del libro:");
+newBook.title = prompt("Ingresa el título del libro:");
+newBook.date = prompt("Ingresa la fecha de hoy");
+newBook.finished = confirm("¿Has terminado de leer el libro? (OK = sí, Cancelar = no)");
+
+console.log(newBook);
