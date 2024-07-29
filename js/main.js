ecee10316fc6d75bedc33f3fc6879137fb1e0bbd
anabrollan <anabrollan@gmail.com>
2024-07-29 15:33:04 -0400

    Agrego un saludo personalizado

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const userName = prompt("Por favor, ingresa tu nombre:");
+
+alert(userName + ", te doy la bienvenida a esta aplicación web :)");
+
+console.log(userName);
+
+
 function Book(author, title, date, finished) {
     this.author = author;
     this.title = title;
@@ -5,11 +12,11 @@ function Book(author, title, date, finished) {
     this.finished = finished;
   }
   
-  const newBook = new Book(
+  const addBook = new Book(
     prompt("Ingresa el autor del libro:"),
     prompt("Ingresa el título del libro:"),
     prompt("Ingresa la fecha de hoy"),
     confirm("¿Has terminado de leer el libro? (OK = sí, Cancelar = no)")
   );
   
-  console.log(newBook);
+  console.log(addBook);
