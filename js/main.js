28c6785de0530347225a398ffad4800b5c10c8b1
anabrollan <anabrollan@gmail.com>
2024-07-30 12:27:44 -0400

    Convierto el objeto en clase

--- js/main.js
+++ js/main.js
@@ -1,30 +1,47 @@
 let userName;
 do {
-  userName = prompt("Por favor, ingresa tu nombre:");
-} while (userName === "");
+userName = prompt("Por favor, ingresa tu nombre:");
+} while (userName === "" || userName === null);
 
 alert("¡Hola, " + userName + "! Te doy la bienvenida a esta aplicación de seguimiento de lecturas :)");
 
 console.log(userName);
 
-function Book(author, title, date, finished) {
-    this.author = author;
-    this.title = title;
-    this.date = date;
-    this.finished = finished;
-  }
-  
-  const newBook = new Book(
-    prompt("Ingresa el autor del libro:"),
-    prompt("Ingresa el título del libro:"),
-    prompt("Ingresa la fecha de hoy"),
-    confirm("¿Has terminado de leer el libro? (OK = sí, Cancelar = no)")
-  );
+class Book {
+    constructor(author, title, date, finished) {
+        this.author = author;
+        this.title = title;
+        this.date = date;
+        this.finished = finished;
+    }
+}
 
-  const finish = "salir";
-  
-  console.log(newBook); 
+let author, title, date, finished;
 
-  do {
+do {
+    author = prompt("Ingresa el autor/-a del libro");
+} while (author === "" || author === null);
+
+do {
+    title = prompt("Ingresa el título del libro");
+} while (title === "" || title === null);
+
+do {
+    date = prompt("Ingresa la fecha de hoy");
+} while (date === "" || date === null);
+
+finished = confirm("¿Has terminado de leer el libro? (OK = sí, Cancelar = no)");
+
+let newBook = new Book(author, title, date, finished);
+
+console.log(newBook);
+
+
+
+let finish = "salir";
+
+console.log(newBook); 
+
+do {
     newBook;
-  } while (newBook === null);
+} while (newBook === null);
